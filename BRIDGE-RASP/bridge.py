46f3c7df3d16c55db4e4ad50becc4d607022c4fa
Truong Quoc Huy <huy2002dd@gmail.com>
2025-06-13 22:12:35 +0700

    rename file

--- BRIDGE-RASP/bridge.py
+++ BRIDGE-RASP/bridge.py
@@ -1,3 +1,5 @@
+
+#This file acts as an intermediary for transmitting data between the laptop and the STM32 via MQTT, TCP, and UART
 import cv2
 import numpy
 import socket
@@ -5,25 +7,25 @@ import struct
 import paho.mqtt.client as mqtt
 from TfLunaI2C import TfLunaI2C
 import serial
-HOST = '192.168.65.91'  # Địa chỉ IP của laptop
-PORT = 9999         # Cổng kết nối
+HOST = '192.168.65.91'  
+PORT = 9999         
 
 # Tạo một socket TCP
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))  # Kết nối tới server
 print('Now starting to send frames and distance data...')
 
-# Khởi tạo UART với baudrate 9600
-uart = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)  # Đổi "/dev/ttyAMA0" thành cổng serial của bạn
+# Khởi tạo UART 
+uart = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)  
 res = []
 # MQTT Configuration
 broker_address = "192.168.65.63"
 topic = "robot/control"
 def on_message(client, userdata, message):
     try:
-        # Parse the received message (format: "w1,w2,w3,w4")
+        # Parse the received message 
         data = message.payload.decode("utf-8")
-        print(f"Received data via MQTT: {data[1]}")  # In dữ liệu ra terminal
+        print(f"Received data via MQTT: {data[1]}")  
         print(f"Received data via MQTT: {data[2]}")
         print(f"Received data via MQTT: {data[3]}")
         print(f"Received data via MQTT: {data[4]}") 
@@ -36,7 +38,7 @@ def on_message(client, userdata, message):
 
             # Send the message via UART
             uart.write(uart_message.encode())
-            print(f"Sent to UART: {uart_message}")  # In dữ liệu gửi qua UART
+            print(f"Sent to UART: {uart_message}")  
              # Read the response from UART
             uart_response = uart.readline().decode('utf-8').strip()
             res =  uart_response
@@ -54,9 +56,9 @@ def on_message(client, userdata, message):
 def measure_distance():
     tfluna = TfLunaI2C()
     tfluna.read_data()
-    if tfluna.dist >= 0:  # Biên độ đáng tin cậy
+    if tfluna.dist >= 0:  
         return tfluna.dist
-    return -1  # Trả về -1 nếu biên độ không đáng tin cậy
+    return -1  
 
 # Lấy thiết bị camera
 capture = cv2.VideoCapture(0)
@@ -71,28 +73,25 @@ try:
     while True:
         # 1. Gửi dữ liệu khoảng cách
         distance = measure_distance()
-        header = b'D'  # Header dữ liệu khoảng cách
-        client.sendall(header + struct.pack('f', distance))  # Gửi khoảng cách dạng float
+        header = b'D'  
+        client.sendall(header + struct.pack('f', distance)) 
         print(f"Khoảng cách đã gửi: {distance} cm")
-        # 3. Gửi dữ liệu khoảng cách
-        # response = on_message()
-        header = b'O'  # Header dữ liệu khoảng cách
+        header = b'O'  
         packed_data = struct.pack(f'{len(res)}f', *res)
         client.sendall(header + packed_data)
-        # client.sendall(header + struct.pack('f', res))  # Gửi khoảng cách dạng float
         print(f" đã gửi: {res}")
         
         # 2. Gửi dữ liệu khung hình video
         success, frame = capture.read()
         while not success and frame is None:
-            success, frame = capture.read()  # Lấy khung hình video
+            success, frame = capture.read()  
 
         # Mã hóa hình ảnh
         _, imgencode = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
         img_data = imgencode.tobytes()
 
         # Gửi header và dữ liệu hình ảnh
-        header = b'I'  # Header dữ liệu hình ảnh
+        header = b'I'  
         client.sendall(header + struct.pack('i', len(img_data)) + img_data)
         print("Khung hình đã gửi.")
 
@@ -101,7 +100,6 @@ try:
 except Exception as e:
     print(f"Lỗi: {e}")
 finally:
-    # Gửi chuỗi +0.00,+0.00,+0.00,+0.00 trước khi thoát
     reset_message = "+0.00,+0.00,+0.00,+0.00\n"
     try:
         uart.write(reset_message.encode())
@@ -109,8 +107,8 @@ finally:
     except Exception as e:
         print(f"Error sending reset message: {e}")
     # Ngừng các kết nối và tài nguyên
-    mqtt_client.loop_stop()  # Ngừng luồng MQTT
-    mqtt_client.disconnect()  # Ngắt kết nối MQTT
+    mqtt_client.loop_stop()  
+    mqtt_client.disconnect()  
     uart.close()
     capture.release()
     client.close()
